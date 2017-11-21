6850bab9ff206830b8199fa101c183d67669edb3
Victor Domingos <victordomingos@users.noreply.github.com>
2017-11-21 13:12:10 +0000

    Update app_settings.py

--- app_settings.py
+++ app_settings.py
@@ -9,15 +9,17 @@ Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
 
 # ------- Configurar estas variáveis antes de usar. ---------
 
+
 # Caminho completo para a pasta que contém os ficheiros e pastas a arquivar e copiar.
 INPUT_FOLDER = '~/Documents/NPK-Backup/db-backup-test/'
 
-# Caminho completo para o arquivo zip temporário a criar para o upload.
-ARCHIVE_NAME = '~/Documents/NPK-Backup/db-backup__'
+# Caminho para o arquivo zip temporário a criar para o upload.
+ARCHIVE_PATH = '~/Documents/NPK-Backup/tmp/'
+ARCHIVE_FILE_NAME = "db-backup__"
 
 # Caminho de destino na Dropbox para o arquivo a carregar (serão acrescentados no 
 # final o carimbo de data/hora e a extensão '.zip').
-REMOTE_PATH = '/db-backup__'
+REMOTE_PATH = '/backups/db-backup__'
 
 # Caminho completo para o ficheiro de registo
 BACKUP_LOG_FILE = '~/Documents/NPK-Backup/backup-log.txt'
