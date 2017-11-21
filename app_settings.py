7ba7992dc843072e12d2e45f4a2dde6f3ff18624
Victor Domingos <victordomingos@users.noreply.github.com>
2017-11-21 11:05:44 +0000

    Create app_settings.py

---
+++ app_settings.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3.6
+# encoding: utf-8
+"""
+Este módulo é parte integrante da aplicação NPK-Backup, desenvolvida por
+Victor Domingos e distribuída sob os termos da licença Creative Commons
+Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+"""
+
+
+# ------- Configurar estas variáveis antes de usar. ---------
+
+# Caminho completo para a pasta que contém os ficheiros e pastas a arquivar e copiar.
+INPUT_FOLDER = '~/Documents/NPK-Backup/db-backup-test/'
+
+# Caminho completo para o arquivo zip temporário a criar para o upload.
+ARCHIVE_NAME = '~/Documents/NPK-Backup/db-backup__'
+
+# Caminho de destino na Dropbox para o arquivo a carregar (serão acrescentados no 
+# final o carimbo de data/hora e a extensão '.zip').
+REMOTE_PATH = '/db-backup__'
+
+# Caminho completo para o ficheiro de registo
+BACKUP_LOG_FILE = '~/Documents/NPK-Backup/backup-log.txt'
+
+# Token de acesso ao Dropbox
+TOKEN = 'GetYourAppTokenFromDropboxAndInsertItHere'
+
+# ----------------------------------------------------------
