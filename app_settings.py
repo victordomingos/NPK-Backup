36d0c875091d4e01551091ecf2a270c941951142
Victor Domingos <victordomingos@users.noreply.github.com>
2017-11-22 22:24:30 +0000

    ajustes

--- app_settings.py
+++ app_settings.py
@@ -10,20 +10,20 @@ Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
 # ------- Configurar estas variáveis antes de usar. ---------
 
 
-# Caminho completo para a pasta que contém os ficheiros e pastas a arquivar e 
+# Caminho completo para a pasta que contém os ficheiros e pastas a arquivar e
 # copiar (com '/' no final).
-INPUT_FOLDER = '~/Documents/NPK-Backup/db-backup-test/'
+INPUT_FOLDER = '~/Dropbox/Aplicativos/Filemaker_backups/NPK-Backup/db-backup/'
 
 # Caminho para o arquivo zip temporário a criar para o upload (com '/' no final).
-ARCHIVE_PATH = '~/Documents/NPK-Backup/tmp/'
+ARCHIVE_PATH = '~/Dropbox/Aplicativos/Filemaker_backups/tmp/'
 ARCHIVE_FILE_NAME = "db-backup__"
 
 # Caminho de destino na Dropbox para o arquivo a carregar (serão acrescentados no 
 # final o carimbo de data/hora e a extensão '.zip'). Sem '/' no final.
-REMOTE_PATH = '/backups/db-backup__'
+REMOTE_PATH = '/backups'
 
 # Caminho completo para o ficheiro de registo
-BACKUP_LOG_FILE = '~/Documents/NPK-Backup/tmp/backup-log.pkl'
+BACKUP_LOG_FILE = '~/Dropbox/Aplicativos/Filemaker_backups/tmp/backup-log.pkl'
 
 # Token de acesso ao Dropbox
 TOKEN = 'GetYourAppTokenFromDropboxAndInsertItHere'
