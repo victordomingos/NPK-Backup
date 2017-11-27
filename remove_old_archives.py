66f73aa73aa3a2768060ee3eb87b0dc2b1f1d4e1
Victor Domingos <victordomingos@users.noreply.github.com>
2017-11-27 23:37:59 +0000

    actually deleting folders

--- remove_old_archives.py
+++ remove_old_archives.py
@@ -19,6 +19,9 @@ Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
 
 import os
 import datetime
+import shutil
+
+from send2trash import send2trash
 
 from app_settings_remove_old import BACKUPS_FOLDER, BACKUP_TIMEOUT, USE_TRASH
 
@@ -34,12 +37,13 @@ def delete_dir_by_date(main_path, year, month=None, day=None):
         full_path = '{}/{}'.format(full_path, month)
     if day is not None:
         full_path = '{}/{}'.format(full_path, day)
-        
-    # TODO...
+
     if USE_TRASH:
         print('Sending directory to Trash:', full_path)
+        send2trash(full_path)
     else:
         print('Deleting directory:', full_path)
+        shutil.rmtree(full_path, ignore_errors=True, onerror=None)
 
 
 for pasta_ano in os.listdir(pasta_principal):
@@ -59,7 +63,7 @@ for pasta_ano in os.listdir(pasta_principal):
             continue  # passa para a pasta de mes seguinte
 
         # se pasta_mes == mes, então continua a processar o seu conteúdo...
-        pasta_mes_atual = pasta_principal + '/' +  pasta_ano + '/' + pasta_mes
+        pasta_mes_atual = pasta_principal + '/' + pasta_ano + '/' + pasta_mes
         for pasta_dia in os.listdir(pasta_mes_atual):
             if int(pasta_dia) < dia:
                 delete_dir_by_date(pasta_principal, pasta_ano, pasta_mes, pasta_dia)
