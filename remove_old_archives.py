37d9df5bdbe5dcea87ea3e140eb3a59acbc98760
Victor Domingos <victordomingos@users.noreply.github.com>
2017-11-29 14:52:34 +0000

    Added logging stuff

--- remove_old_archives.py
+++ remove_old_archives.py
@@ -20,15 +20,37 @@ Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
 import os
 import datetime
 import shutil
+import logging
+import logging.handlers
 
 from send2trash import send2trash
 
-from app_settings_remove_old import BACKUPS_FOLDER, BACKUP_TIMEOUT, USE_TRASH
+from app_settings_remove_old import *
 
+
+logger = logging.getLogger()
 
 pasta_principal = os.path.expanduser(BACKUPS_FOLDER)
+log_path = os.path.expanduser(LOGS_PATH)
 timeout = datetime.datetime.now() - datetime.timedelta(days=BACKUP_TIMEOUT)
 ano, mes, dia = timeout.year, timeout.month, timeout.day
+
+
+def setup_logging():
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
+    
+    # Write log messages to a file
+    file = logging.FileHandler(log_path)
+    file.setLevel(logging.DEBUG)
+    file.setFormatter(formatter)
+    logger.addHandler(file)
+    
+    # Show log messages also on screen
+    screen = logging.StreamHandler()
+    screen.setLevel(logging.DEBUG)
+    screen.setFormatter(formatter)
+    logger.addHandler(screen)
 
 
 def delete_dir_by_date(main_path, year, month=None, day=None):
@@ -39,55 +61,66 @@ def delete_dir_by_date(main_path, year, month=None, day=None):
         full_path = '{}/{}'.format(full_path, day)
 
     if USE_TRASH:
-        print('Sending directory to Trash:', full_path)
+        msg = 'Sending directory to Trash: ' + full_path
+        logger.debug(msg)
         send2trash(full_path)
     else:
-        print('Deleting directory:', full_path)
+        msg = 'Deleting directory: ' + full_path
+        logger.debug(msg)
         shutil.rmtree(full_path, ignore_errors=True, onerror=None)
 
 
-for pasta_ano in os.listdir(pasta_principal):
-    if pasta_ano.startswith('.'):
-        continue
-    try:
-        if int(pasta_ano) < ano:
-            delete_dir_by_date(pasta_principal, pasta_ano)
-            continue
-        elif int(pasta_ano) > ano:
-            continue  # passa para a pasta de ano seguinte
-    except Exception as e:
-        print("An exception occurred while iterating through the folders.")
-        print(e)
-        print("Continuing with next folder...")
-        continue
+def main():
+
+    setup_logging()
     
-    # se pasta_ano == ano, então continua a processar o seu conteúdo...
-    pasta_ano_atual = pasta_principal + '/' + pasta_ano
-    for pasta_mes in os.listdir(pasta_ano_atual):
-        if pasta_mes.startswith('.'):
+    
+    for pasta_ano in os.listdir(pasta_principal):
+        if pasta_ano.startswith('.'):
             continue
         try:
-            if int(pasta_mes) < mes:
-                delete_dir_by_date(pasta_principal, pasta_ano, pasta_mes)
+            if int(pasta_ano) < ano:
+                delete_dir_by_date(pasta_principal, pasta_ano)
                 continue
-            elif int(pasta_mes) > mes:
-                continue  # passa para a pasta de mes seguinte
+            elif int(pasta_ano) > ano:
+                continue  # passa para a pasta de ano seguinte
         except Exception as e:
-            print("An exception occurred while iterating through the folders.")
-            print(e)
-            print("Continuing with next folder...")
+            msg = 'An exception occurred while iterating through the folders.\n' + str(e)
+            logger.debug(msg)
+            logger.debug("Continuing with next folder...")
             continue
-
-        # se pasta_mes == mes, então continua a processar o seu conteúdo...
-        pasta_mes_atual = pasta_principal + '/' + pasta_ano + '/' + pasta_mes
-        for pasta_dia in os.listdir(pasta_mes_atual):
-            if pasta_dia.startswith('.'):
+        
+        # se pasta_ano == ano, então continua a processar o seu conteúdo...
+        pasta_ano_atual = pasta_principal + '/' + pasta_ano
+        for pasta_mes in os.listdir(pasta_ano_atual):
+            if pasta_mes.startswith('.'):
                 continue
             try:
-                if int(pasta_dia) < dia:
-                    delete_dir_by_date(pasta_principal, pasta_ano, pasta_mes, pasta_dia)
+                if int(pasta_mes) < mes:
+                    delete_dir_by_date(pasta_principal, pasta_ano, pasta_mes)
+                    continue
+                elif int(pasta_mes) > mes:
+                    continue  # passa para a pasta de mes seguinte
             except Exception as e:
-                print("An exception occurred while iterating through the folders.")
-                print(e)
-                print("Continuing with next folder...")
+                msg = 'An exception occurred while iterating through the folders.\n' + str(e)
+                logger.debug(msg)
+                logger.debug("Continuing with next folder...")
                 continue
+    
+            # se pasta_mes == mes, então continua a processar o seu conteúdo...
+            pasta_mes_atual = pasta_principal + '/' + pasta_ano + '/' + pasta_mes
+            for pasta_dia in os.listdir(pasta_mes_atual):
+                if pasta_dia.startswith('.'):
+                    continue
+                try:
+                    if int(pasta_dia) < dia:
+                        delete_dir_by_date(pasta_principal, pasta_ano, pasta_mes, pasta_dia)
+                except Exception as e:
+                    msg = 'An exception occurred while iterating through the folders.\n' + str(e)
+                    logger.debug(msg)
+                    logger.debug("Continuing with next folder...")
+                    continue
+
+
+if __name__ == "__main__":
+    main()
