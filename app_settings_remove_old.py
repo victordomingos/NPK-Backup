feeb29f5d8b68c1cb1cfc256313061255905ccf9
Victor Domingos <victordomingos@users.noreply.github.com>
2017-11-29 14:49:59 +0000

    Update app_settings_remove_old.py

--- app_settings_remove_old.py
+++ app_settings_remove_old.py
@@ -12,7 +12,7 @@ Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
 
 # Caminho completo para a pasta que contém os ficheiros e pastas a arquivar e
 # copiar (sem '/' no final).
-BACKUPS_FOLDER = '~/Documents/NPK-Backup/delete-tests'
+BACKUPS_FOLDER = '~/Dropbox/Aplicativos/backups'
 
 # Número de dias que as cópias de segurança devem ser conservadas sem apagar.
 BACKUP_TIMEOUT = 5
@@ -20,4 +20,5 @@ BACKUP_TIMEOUT = 5
 # Usar 'Lixo'/'Reciclagem' do sistema operativo em vez de apagar imediatamente?
 USE_TRASH = True
 
+LOGS_PATH = '~/Dropbox/npk-backup.log'
 # ----------------------------------------------------------
